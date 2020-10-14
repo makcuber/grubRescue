2a9d5054f2fc76c85b411eacaaf16b8343e822f8
Jonathan Brunath <jbrunath@tucows.com>
2020-10-14 19:54:38 -0400

    Added basic function structure

--- grubRescue.sh
+++ grubRescue.sh
@@ -1,18 +1,62 @@
 #!/bin/bash
 
-echo "Mounting root partition..."
-sudo mount /dev/nvme0n1p4 /mnt &
+bootPart="/dev/nvme0n1p1"
+rootPart="/dev/nvme0n1p4"
+delay=1
 
-echo "Mounting core fs..."
-sudo mount --bind /dev /mnt/dev &
-sudo mount --bind /dev/pts /mnt/dev/pts &
-sudo mount --bind /proc /mnt/proc &
-sudo mount --bind /sys /mnt/sys &
+#set -e #enable exit on error
 
-echo "Mounting boot partition..."
-sudo mount /dev/nvme0n1p1 /mnt/boot/efi &
+function mountFS { 
+	echo "Mounting root partition as symlink..."
+	sudo mount "$rootPart" /mnt &
+	sleep "$delay"
+	
+	echo "Mounting core fs as symlink..."
+	sudo mount --bind /dev /mnt/dev
+	sleep "$delay"
+	sudo mount --bind /dev/pts /mnt/dev/pts &
+	sleep "$delay"
+	sudo mount --bind /proc /mnt/proc &
+	sleep "$delay"
+	sudo mount --bind /sys /mnt/sys &
+	sleep "$delay"
+	
+	echo "Mounting boot partition as symlink..."
+	sudo mount /dev/nvme0n1p1 /mnt/boot/efi &
+	sleep "$delay"
+}
+function chrootExe {
+	echo "Entering fake root env..."
+	#"set -e" enables exit on error
+	sudo chroot /mnt /bin/bash -c "set -e && grub-install $bootPart && update-grub && exit"
+}
+function unmountFS {
+	echo "Unmounting symlinked boot partition..."
+	sudo umount /mnt/boot/efi
+	sleep "$delay"
+	
+	echo "Unmounting symlinked core fs..."
+	sudo umount /mnt/sys
+	sleep "$delay"
+	sudo umount /mnt/proc
+	sleep "$delay"
+	sudo umount /mnt/dev/pts
+	sleep "$delay"
+	sudo umount /mnt/dev
+	sleep "$delay"
+	
+	echo "Unmount symlinked root partition..."
+	sudo umount /mnt
+	sleep "$delay"
+}
 
-echo "Entering fake root env..."
-sudo chroot /mnt
-
+function run {
+	echo "Ensuring symlink fs is not mounted..."
+	unmountFS
+	mountFS
+	chrootExe
+	unmountFS
+}
+run
 echo "Done."
+
